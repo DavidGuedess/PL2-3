c6fc53d56a961e3faf4b7326a7dd01c487dde0dc
ddfigueiras <diogo.figueiras.silva@gmail.com>
2026-03-17 15:21:04 +0000

    SCRUM-42: estrutura inicial endpoint /login

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  res.status(200).json({
+    message: 'Login endpoint',
+    email
+  });
+};
